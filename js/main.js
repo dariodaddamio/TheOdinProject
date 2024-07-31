cc279ebec893de5e1ad0ee14f4b7f008100a5b29
dariodaddamio <dario.daddamio@gmail.com>
2024-07-30 22:59:49 -0700

    completed fade in & out on first load + homepage + starting to add content to all

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 window.onload = function() {
     const logo = document.querySelector('.logo');
-    logo.classList.add('fade-in');
-    logo.classList.add('shrink');
+    if (logo) {
+        logo.classList.add('fade-in');
+        setTimeout(function() {
+            logo.addEventListener('click', function() {
+                logo.classList.add('fade-out');
+                setTimeout(function() {
+                    window.location.href = 'homepage.html';
+                }, 1000);
+            });
+        }, 0);
+    }
+        
+    const navLogo = document.querySelector('.nav-logo');
+    if (navLogo) {
+        setTimeout(function() {
+            navLogo.addEventListener('click', function() {
+                window.location.href = 'homepage.html';
+            });
+        }, 100);
+    }
 }
